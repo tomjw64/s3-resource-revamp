3ab177f6757f6620d82595116e97a0144290fe10
tomjw64 <tom.jw64@gmail.com>
2019-11-22 01:23:03 -0600

    Add utils tests

---
+++ test/test_utils.py
@@ -0,0 +1,49 @@
+from src import utils
+
+class TestUtils:
+    def test_deep_get(self):
+        test_dict = {
+            'a': {
+                'b': {
+                    'c': 'You found me!'
+                }
+            }
+        }
+        result = utils.deep_get(test_dict, 'a', 'b', 'c')
+        assert result == 'You found me!'
+
+
+    def test_deep_get_returns_none_when_not_found(self):
+        test_dict = {
+            'a': {
+                'b': {
+                    'c': 'You found me!'
+                }
+            }
+        }
+        result = utils.deep_get(test_dict, 'a', 'NOPE', 'c')
+        assert result is None
+
+
+    def test_deep_get_default_used_if_not_found(self):
+        test_dict = {
+            'a': {
+                'b': {
+                    'c': 'You found me!'
+                }
+            }
+        }
+        result = utils.deep_get(test_dict, 'a', 'NOPE', 'c', default='Maybe next time!')
+        assert result == 'Maybe next time!'
+
+
+    def test_deep_get_default_unused_if_found(self):
+        test_dict = {
+            'a': {
+                'b': {
+                    'c': 'You found me!'
+                }
+            }
+        }
+        result = utils.deep_get(test_dict, 'a', 'b', 'c', default='Maybe next time!')
+        assert result == 'You found me!'
