c6e2b77dd432c01788a59f6eef8c51a548179b7f
tomjw64 <tom.jw64@gmail.com>
2019-12-02 12:22:23 -0600

    If no version provided, check returns latest

--- src/check.py
+++ src/check.py
@@ -62,9 +62,9 @@ def check(in_stream):
     input = json.load(in_stream)
 
     service = 's3'
-    region = deep_get(input, 'source', 'region')
-    bucket = deep_get(input, 'source', 'bucket')
-    endpoint = deep_get(input, 'source', 'endpoint', default='http://s3.amazonaws.com')
+    region = deep_get(input, 'source', 'service', 'region')
+    bucket = deep_get(input, 'source', 'service', 'bucket')
+    endpoint = deep_get(input, 'source', 'service', 'endpoint', default='http://s3.amazonaws.com')
 
     access_key_id = deep_get(input, 'source', 'credentials', 'access_key_id')
     secret_access_key = deep_get(input, 'source', 'credentials', 'secret_access_key')
@@ -92,9 +92,9 @@ def check(in_stream):
     response_objects = json.loads(response)['Contents']
     matching_objects = list(filter(key_regexp_matches(regexp_filter), response_objects))
 
-    if version_filter is None:
+    if version_filter == 'every':
         new_versions = matching_objects
-    elif version_filter == 'latest':
+    elif version_filter is None or version_filter == 'latest':
         new_versions = reduce(key_regexp_max_version(regexp_filter), matching_objects, [])
     else:
         threshold_semver = parse_semver_array_from_string(version_filter)
