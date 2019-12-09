fa4ac0bf546ec0849fb42f42bba1232060e0850c
tomjw64 <tom.jw64@gmail.com>
2019-12-09 10:23:46 -0600

    Add in action

--- src/action_in.py
+++ src/action_in.py
@@ -1,1 +1,51 @@
+import json
+import sys
+from pathlib import Path
+
+from .resource_boto_client import ResourceBotoClient
+from .utils import eprint
+
+
 def action_in(dest_path, in_stream):
+    input = json.load(in_stream)
+    client = ResourceBotoClient(input)
+    action_mode = client.action_mode
+    check_version = client.version_key
+
+    eprint('Mode: ' + action_mode)
+    eprint('Check version: ' + check_version)
+
+    def download_check_version():
+        destination = Path(dest_path) / check_version
+        client.download_file(
+            key=check_version,
+            destination=str(destination))
+        eprint('Object downloaded: ' + check_version)
+        return {'version': {'key': check_version}}
+
+    def sync_filtered():
+        object_keys = client.list_filtered_objects()
+        for object_key in object_keys:
+            destination = Path(dest_path) / object_key['key']
+            if not destination.exists():
+                client.download_file(
+                    key=object_key['key'],
+                    destination=str(destination))
+
+        eprint('Objects downloaded: ' + str(object_keys))
+        # This is a garbage value
+        return {'version': {'key': check_version}}
+
+    return {
+        'all': sync_filtered,
+        'single': download_check_version
+    }[action_mode]()
+
+
+def main():  # pragma: no cover
+    dest_path = sys.argv[1]
+    print(json.dumps(action_in(dest_path, sys.stdin)))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
---
+++ test/test_in.py
@@ -0,0 +1,260 @@
+import pytest
+
+from src import action_in
+
+from .helpers import (read_json_file_as_dict,
+                      make_stream,
+                      mock_s3_client)
+
+
+DIR_NO_EXIST = '_tmp-delete-me'
+
+
+class TestIn:
+    def test_in_empty_bucket(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-empty.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'file.txt'
+            },
+            'params': {
+                'mode': 'single'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+        mock_client.download_file.assert_called_once_with(
+            Bucket=None,
+            Key='file.txt',
+            Filename=f'{DIR_NO_EXIST}/file.txt')
+
+    def test_in_exact_match_latest(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-minio.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'file.txt'
+            },
+            'source': {
+                'filters': {
+                    'regexp': 'file.txt'
+                }
+            },
+            'params': {
+                'mode': 'single'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+        mock_client.download_file.assert_called_once_with(
+            Bucket=None,
+            Key='file.txt',
+            Filename=f'{DIR_NO_EXIST}/file.txt')
+
+    def test_in_bad_version_single(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-minio.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'file.txt'
+            },
+            'source': {
+                'filters': {
+                    'regexp': 'file.txt',
+                    'version': 'asdf'
+                }
+            },
+            'params': {
+                'mode': 'single'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+        mock_client.download_file.assert_called_once_with(
+            Bucket=None,
+            Key='file.txt',
+            Filename=f'{DIR_NO_EXIST}/file.txt')
+
+    def test_in_bad_version_all(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-minio.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'file.txt'
+            },
+            'source': {
+                'filters': {
+                    'regexp': 'file.txt',
+                    'version': 'asdf'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        with pytest.raises(ValueError):
+            action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+    def test_in_multiple_match_latest(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-minio-multiple-match.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'file.text'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'file.te?xt',
+                    'version': 'latest'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+        mock_client.download_file.assert_called_once_with(
+            Bucket=None,
+            Key='file.text',
+            Filename=f'{DIR_NO_EXIST}/file.text')
+
+    def test_in_multiple_match(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-minio-multiple-match.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'file.text'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'file.te?xt',
+                    'version': 'every'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+        assert mock_client.download_file.call_count == 2
+
+    def test_in_pretend_module_ids_are_versions_named_capture(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-full-bucket.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'book/m63248/index.cnxml'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'book/m(?P<version>\d+)/index[.].*',
+                    'version': 'every'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+        assert mock_client.download_file.call_count == 159
+
+    def test_in_pretend_module_ids_are_versions_named_capture_single(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-full-bucket.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'book/m63248/index.cnxml'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'book/m(?P<version>\d+)/index[.].*',
+                    'version': 'every'
+                }
+            },
+            'params': {
+                'mode': 'single'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+        mock_client.download_file.assert_called_once_with(
+            Bucket=None,
+            Key='book/m63248/index.cnxml',
+            Filename=f'{DIR_NO_EXIST}/book/m63248/index.cnxml')
+
+    def test_in_pretend_module_ids_are_versions_multi_group_capture(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-full-bucket.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'book/m63248/index.cnxml'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'book/m(?P<version>\d+)/index[.].*',
+                    'version': '0'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+        assert mock_client.download_file.call_count == 159
+
+    def test_in_pretend_module_ids_are_versions_threshold(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-full-bucket.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'book/m63248/index.cnxml'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'book/m(?P<version>\d+)/index[.].*',
+                    'version': '63000'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+        assert mock_client.download_file.call_count == 2
+
+    def test_in_pretend_module_ids_are_versions_latest(self, mocker):
+        response = read_json_file_as_dict('list-objects-v2-full-bucket.json')
+        mock_client = mock_s3_client(mocker, list_response=response)
+        mocker.patch('boto3.client', return_value=mock_client)
+        input = {
+            'version': {
+                'key': 'book/m63248/index.cnxml'
+            },
+            'source': {
+                'filters': {
+                    'regexp': r'book/m(?P<version>\d+)/index[.].*',
+                    'version': 'latest'
+                }
+            },
+            'params': {
+                'mode': 'all'
+            }
+        }
+        action_in.action_in(DIR_NO_EXIST, make_stream(input))
+
+        mock_client.download_file.assert_called_once_with(
+            Bucket=None,
+            Key='book/m63248/index.cnxml',
+            Filename=f'{DIR_NO_EXIST}/book/m63248/index.cnxml')
