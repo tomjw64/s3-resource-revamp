e38747e67a4e50ef040c8afd249bd2523cd4115a
tomjw64 <tom.jw64@gmail.com>
2019-12-09 10:19:59 -0600

    Offload s3 actions into resource_boto_client to be reused

--- src/action_check.py
+++ src/action_check.py
@@ -1,59 +1,14 @@
 import json
 import sys
-import boto3
-from functools import reduce
 
-from .utils import (eprint,
-                    deep_get,
-                    parse_semver_array_from_string,
-                    key_regexp_matches,
-                    key_regexp_max_version,
-                    key_regexp_version_not_less_than)
+from .static_boto_client import ResourceBotoClient
+from .utils import eprint
 
 
 def action_check(in_stream):
     input = json.load(in_stream)
-
-    service = 's3'
-    region = deep_get(input, 'source', 'service', 'region')
-    bucket = deep_get(input, 'source', 'service', 'bucket')
-    endpoint = deep_get(input, 'source', 'service', 'endpoint', default='http://s3.amazonaws.com')
-
-    access_key_id = deep_get(input, 'source', 'credentials', 'access_key_id')
-    secret_access_key = deep_get(input, 'source', 'credentials', 'secret_access_key')
-
-    prefix_filter = deep_get(input, 'source', 'filters', 'prefix', default='')
-    regexp_filter = deep_get(input, 'source', 'filters', 'regexp')
-    version_filter = deep_get(input, 'source', 'filters', 'version')
-
-    s3_client = boto3.client(
-        service_name=service,
-        region_name=region,
-        aws_access_key_id=access_key_id,
-        aws_secret_access_key=secret_access_key,
-        endpoint_url=endpoint)
-
-    response = s3_client.list_objects_v2(
-        Bucket=bucket,
-        Prefix=prefix_filter)
-
-    if response == {} or not regexp_filter:
-        eprint('No versions found')
-        return []
-
-    response_objects = response['Contents']
-    matching_objects = list(filter(key_regexp_matches(regexp_filter), response_objects))
-
-    if version_filter == 'every':
-        filtered_objects = matching_objects
-    elif version_filter is None or version_filter == 'latest':
-        filtered_objects = reduce(key_regexp_max_version(regexp_filter), matching_objects, [])
-    else:
-        threshold_semver = parse_semver_array_from_string(version_filter)
-        filtered_objects = list(filter(key_regexp_version_not_less_than(regexp_filter, threshold_semver),
-                                matching_objects))
-
-    object_keys = [{'key': obj['Key']} for obj in filtered_objects]
+    client = StaticBotoClient(input)
+    object_keys = client.list_filtered_objects()
     eprint('Versions found: ' + str(object_keys))
     return object_keys
 
---
+++ src/resource_boto_client.py
@@ -0,0 +1,90 @@
+import boto3
+import botocore
+from functools import reduce
+
+from .utils import (eprint,
+                    deep_get,
+                    parse_semver_array_from_string,
+                    key_regexp_matches,
+                    key_regexp_max_version,
+                    key_regexp_version_not_less_than)
+
+
+class ResourceBotoClient:
+    def __init__(self, input):
+        self.init_source_options(input)
+        self.init_version(input)
+        self.init_params(input)
+        self.client = boto3.client(
+            service_name=self.service,
+            region_name=self.region,
+            aws_access_key_id=self.access_key_id,
+            aws_secret_access_key=self.secret_access_key,
+            endpoint_url=self.endpoint)
+
+    def init_source_options(self, input):
+        self.service = 's3'
+        self.region = deep_get(input, 'source', 'service', 'region')
+        self.bucket = deep_get(input, 'source', 'service', 'bucket')
+        self.endpoint = deep_get(input, 'source', 'service', 'endpoint', default='http://s3.amazonaws.com')
+
+        self.access_key_id = deep_get(input, 'source', 'credentials', 'access_key_id')
+        self.secret_access_key = deep_get(input, 'source', 'credentials', 'secret_access_key')
+
+        self.prefix_filter = deep_get(input, 'source', 'filters', 'prefix', default='')
+        self.regexp_filter = deep_get(input, 'source', 'filters', 'regexp')
+        self.version_filter = deep_get(input, 'source', 'filters', 'version')
+
+    def init_version(self, input):
+        self.version_key = deep_get(input, 'version', 'key')
+
+    def init_params(self, input):
+        self.action_mode = deep_get(input, 'params', 'mode')
+        self.upload_glob = deep_get(input, 'params', 'glob')
+
+    def list_filtered_objects(self):
+        try:
+            response = self.client.list_objects_v2(
+                Bucket=self.bucket,
+                Prefix=self.prefix_filter)
+        except botocore.errorfactory.NoSuchBucket:  # pragma: no cover
+            eprint('No versions found - no bucket')
+            return []
+
+        if not self.regexp_filter:
+            eprint('No versions found - no regex')
+            return []
+
+        try:
+            response_objects = response['Contents']
+        except KeyError:
+            eprint(f'Cannot read \'Contents\' of {response}')
+            eprint('No versions found - cannot read or none found')
+            return []
+
+        matching_objects = list(filter(key_regexp_matches(self.regexp_filter), response_objects))
+
+        if self.version_filter == 'every':
+            filtered_objects = matching_objects
+        elif self.version_filter in [None, 'latest']:
+            filtered_objects = reduce(key_regexp_max_version(self.regexp_filter), matching_objects, [])
+        else:
+            threshold_semver = parse_semver_array_from_string(self.version_filter)
+            filtered_objects = list(filter(key_regexp_version_not_less_than(self.regexp_filter,
+                                                                            threshold_semver),
+                                    matching_objects))
+
+        object_keys = [{'key': obj['Key']} for obj in filtered_objects]
+        return object_keys
+
+    def download_file(self, *, key, destination):
+        self.client.download_file(
+            Bucket=self.bucket,
+            Key=key,
+            Filename=destination)
+
+    def upload_file(self, *, source, key):
+        self.client.upload_file(
+            Filename=source,
+            Bucket=self.bucket,
+            Key=key)
